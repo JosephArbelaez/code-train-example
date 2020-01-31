cb364083a6faf90f822234b89fd4b1a2b83ba4cc
vn503o9 <josepharbelaez@gmail.com>
2020-01-31 09:21:58 -0600

    Last Push

--- public/input/input.js
+++ public/input/input.js
@@ -3,23 +3,29 @@ let lat, lon;
 if ('geolocation' in navigator) {
         console.log('geolocation available');
         navigator.geolocation.getCurrentPosition(async position => {
+            let lat, lon, weather, air;
             try {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            document.getElementById('latitude').textContent = lat.toFixed(2);
-            document.getElementById('longitude').textContent = lon.toFixed(2);
-            const api_url = `/weather/${lat},${lon}`;
-            const response = await fetch(api_url);
-            const json = await response.json();
-            const weather = json.weather.currently;
-            const air = json.air_quality.results[0].measurements[0];
-            document.getElementById('summary').textContent = weather.summary;
-            document.getElementById('temperature').textContent = weather.temperature;
-            document.getElementById('aq_parameter').textContent = air.parameter;
-            document.getElementById('aq_value').textContent = air.value;
-            document.getElementById('aq_units').textContent = air.unit;
-            document.getElementById('aq_date').textContent = air.lastUpdated;
-            
+                lat = position.coords.latitude;
+                lon = position.coords.longitude;
+                document.getElementById('latitude').textContent = lat.toFixed(2);
+                document.getElementById('longitude').textContent = lon.toFixed(2);
+                const api_url = `/weather/${lat},${lon}`;
+                const response = await fetch(api_url);
+                const json = await response.json();
+                weather = json.weather.currently;
+                air = json.air_quality.results[0].measurements[0];
+                document.getElementById('summary').textContent = weather.summary;
+                document.getElementById('temperature').textContent = weather.temperature;
+                document.getElementById('aq_parameter').textContent = air.parameter;
+                document.getElementById('aq_value').textContent = air.value;
+                document.getElementById('aq_units').textContent = air.unit;
+                document.getElementById('aq_date').textContent = air.lastUpdated;
+            } catch (error) {
+                console.log (error);
+                air = { value: -1 };
+                document.getElementById('aq_value').textContent = "No Reading Available.";
+            }
+
             const data = { lat, lon, weather, air };
             const options = {
                 method: 'POST',
@@ -31,10 +37,6 @@ if ('geolocation' in navigator) {
             const db_response = await fetch('/api', options);
             const db_json = await db_response.json();
             console.log(db_json);
-        } catch (error) {
-            console.log (error);
-            document.getElementById('aq_value').textContent = "No Reading Available.";
-        }
         }); 
     }   else {
             console.log('geolocation not available');
