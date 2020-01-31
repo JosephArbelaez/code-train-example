1303ef15d68ccd0712e442d5e77feb8b9fbf00bc
vn503o9 <josepharbelaez@gmail.com>
2020-01-30 22:35:46 -0600

    Fixed server port issue

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
-const app = express();
 require('dotenv').config();
-
-console.log(process.env);
-// Have the application listen to whichever port the hosting application sets or 3000.
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("Listening at 3000"));
+app.listen(port, () => {
+    console.log(`Starting server at ${port}`);
+});
 app.use(express.static('public'));
 app.use(express.json( {limit: '1mb'}));
 
